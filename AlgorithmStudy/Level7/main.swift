ba9257004f98a3761855a950cef7abfc2e65a0a1
picnicodesm <offiksm0903@naver.com>
2022-07-22 14:44:21 +0900

    2022_07_22

--- AlgorithmStudy/Level7/main.swift
+++ AlgorithmStudy/Level7/main.swift
@@ -1,31 +1,50 @@
-// 2775
+// 10757
 /*
- 평소 반상회에 참석하는 것을 좋아하는 주희는 이번 기회에 부녀회장이 되고 싶어 각 층의 사람들을 불러 모아 반상회를 주최하려고 한다.
-
- 이 아파트에 거주를 하려면 조건이 있는데, “a층의 b호에 살려면 자신의 아래(a-1)층의 1호부터 b호까지 사람들의 수의 합만큼 사람들을 데려와 살아야 한다” 는 계약 조항을 꼭 지키고 들어와야 한다.
-
- 아파트에 비어있는 집은 없고 모든 거주민들이 이 계약 조건을 지키고 왔다고 가정했을 때, 주어지는 양의 정수 k와 n에 대해 k층에 n호에는 몇 명이 살고 있는지 출력하라. 단, 아파트에는 0층부터 있고 각층에는 1호부터 있으며, 0층의 i호에는 i명이 산다.
+ 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
  
- 입력: 첫 번째 줄에 Test case의 수 T가 주어진다. 그리고 각각의 케이스마다 입력으로 첫 번째 줄에 정수 k, 두 번째 줄에 정수 n이 주어진다
- 출력: 각각의 Test case에 대해서 해당 집에 거주민 수를 출력하라.
+ 입력: 첫째 줄에 A와 B가 주어진다. (0 < A,B < 1010000)
+ 출력: 첫째 줄에 A+B를 출력한다.
+ 
+ 에제 입력: 9223372036854775807 9223372036854775808
+ 예제 출력: 18446744073709551615
+ 
+ UInt64를 쓰면 런타임 에러 발생
  */
-
 import Foundation
 
-func getNthPersonNum(floor: Int, home: Int) -> Int {
-    var sum = 0
-    if floor == 0 {
-        return home
-    }
-    for i in 1...home {
-       sum += getNthPersonNum(floor: floor - 1, home: i)
-    }
-    return sum
+let input = readLine()!.split(separator: " ")
+var num1 = input[0].map{ Int(String($0))! }
+var num2 = input[1].map{ Int(String($0))! }
+while num1.first == 0 {
+    num1.removeFirst()
 }
-
-var C = Int(readLine()!)!
-for _ in 1...C {
-    let floor = Int(readLine()!)!
-    let home = Int(readLine()!)!
-    print(getNthPersonNum(floor: floor, home: home))
+while num2.first == 0 {
+    num2.removeFirst()
 }
+num1.reverse()
+num2.reverse()
+var minN = num1
+var maxN = num1
+if num1.count >= num2.count {
+    minN = num2
+    maxN = num1
+} else {
+    minN = num1
+    maxN = num2
+}
+var answer = [String]()
+var add = 0
+for index in 0..<maxN.count {
+    if index < minN.count {
+        answer.append(String((maxN[index]+minN[index] + add ) % 10 ))
+        add = (maxN[index]+minN[index] + add) / 10
+    } else {
+        answer.append(String((maxN[index] + add) % 10))
+        add = (maxN[index]+add) / 10
+    }
+}
+if add == 1 {
+    answer.append(String(add))
+}
+answer.reverse()
+print(answer.joined())
